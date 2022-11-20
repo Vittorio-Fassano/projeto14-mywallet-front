4ccf2f9985145db8c37128a842d19002f93b31c7
Vittorio <vittfassano@gmail.com>
2022-11-20 10:08:45 -0300

    feat: global style

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import {Route, Routes, BrowserRouter} from 'react-router-dom';
+import { useState } from 'react';
+
+import GlobalStyle from '../GlobalStyle/GlobalStyle'; //layout settings used for all screens
+import UserContext from '../contexts/userContext'; //transfer information between components
+
+function App() {
+
+    return (
+        <>
+            <GlobalStyle/>
+
+        </>
+
+
+        )
+}
+
+export default App;
