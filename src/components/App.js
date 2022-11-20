8f84eea13f38f46de678bc1c01355425c4bee05e
Vittorio <vittfassano@gmail.com>
2022-11-20 12:14:28 -0300

    test npm run build for vercel

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,19 @@ import { useState } from 'react';
 import GlobalStyle from '../GlobalStyle/GlobalStyle'; //layout settings used for all screens
 import UserContext from '../contexts/userContext'; //transfer information between components
 
+import SignUp  from './ApplicationAccess/Sign-Up';
+
+
+
 function App() {
+
+    const contextValue = {};
+
 
     return (
             <>
                 <GlobalStyle />
-                <UserContext.Provider value = {conxtextValue}>
+                <UserContext.Provider value = {contextValue}>
                     <BrowserRouter>
                     <Routes>
                         <Route path='/sign-up' element={<SignUp />}/>
---
+++ src/components/ApplicationAccess/Sign-Up.js
@@ -0,0 +1,11 @@
+
+
+
+function SignUp () {
+
+}
+
+export default SignUp;
+
+
+///
